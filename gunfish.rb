541194b35570050818dd8c3325fc95572b390faa
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2019-12-07 07:30:57 +0900

    gunfish v0.4.0

--- gunfish.rb
+++ gunfish.rb
@@ -1,12 +1,16 @@
 class Gunfish < Formula
-  version '0.3.3'
-  homepage 'https://github.com/kayac/Gunfish'
-  url "https://github.com/kayac/Gunfish/releases/download/v0.3.3/gunfish-v0.3.3-darwin-amd64.zip"
-  sha256 '4c30902d4cc6d04be99157b352656358bd8187c130676ba888fb0da96c0183d1'
-  head 'https://github.com/kayac/Gunfish.git'
+  version '0.4.0'
+  homepage 'https://github.com/kayac/gunfish'
+  url "https://github.com/kayac/Gunfish/releases/download/v0.4.0/gunfish-v0.4.0-darwin-amd64"
+  sha256 '4390c7f1c1a196b15440548a73cd864bade671e34129ab84fcbd5554fdba2b1d'
+  head 'https://github.com/kayac/gunfish.git'
+
+  head do
+    depends_on 'go' => :build
+  end
 
   def install
-    system 'mv', 'gunfish-v0.3.3-darwin-amd64', 'gunfish'
+    system 'mv', 'gunfish-v0.4.0-darwin-amd64', 'gunfish'
     bin.install 'gunfish'
   end
 end
