701f6f232bc2af7560e41a9980b23fa2ad3f9604
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2020-01-20 13:49:44 +0900

    ecspresso v0.13.5

--- ecspresso.rb
+++ ecspresso.rb
@@ -1,8 +1,8 @@
 class Ecspresso < Formula
-  version '0.13.4'
+  version '0.13.5'
   homepage 'https://github.com/kayac/ecspresso'
-  url "https://github.com/kayac/ecspresso/releases/download/v0.13.4/ecspresso-v0.13.4-darwin-amd64"
-  sha256 'd2772578cfaf9d716a9502ac622490d2e584eec444b820f9b516f81f9f9286da'
+  url "https://github.com/kayac/ecspresso/releases/download/v0.13.5/ecspresso-v0.13.5-darwin-amd64"
+  sha256 '892aaf41a1c91ae9c498e1d8d51c21d1185243020c974522c8dd12b8b18f1ee9'
   head 'https://github.com/kayac/ecspresso.git'
 
   head do
@@ -13,7 +13,7 @@ class Ecspresso < Formula
     if build.head?
       system 'make', 'build'
     end
-    system 'mv', 'ecspresso-v0.13.4-darwin-amd64', 'ecspresso'
+    system 'mv', 'ecspresso-v0.13.5-darwin-amd64', 'ecspresso'
     bin.install 'ecspresso'
   end
 end
