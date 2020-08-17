5e447803f911a06ee0af5baa55a15774f568533e
ossbot <oss@noreply.kayac.com>
2020-08-17 09:30:09 +0000

    update to newer version

--- ecspresso.rb
+++ ecspresso.rb
@@ -1,8 +1,8 @@
 class Ecspresso < Formula
-  version '0.17.2'
+  version '0.17.3'
   homepage 'https://github.com/kayac/ecspresso'
-  url "https://github.com/kayac/ecspresso/releases/download/v0.17.2/ecspresso-v0.17.2-darwin-amd64"
-  sha256 '348a69aa1a12117de6e85ff2e025fcc80567c451db9481134871914ae46d0edd'
+  url "https://github.com/kayac/ecspresso/releases/download/v0.17.3/ecspresso-v0.17.3-darwin-amd64"
+  sha256 '1ac91503dcf2e7883b9df0d2a5b54c7c9a49e2d7b78f73286cfc19bb6ad44778'
   head 'https://github.com/kayac/ecspresso.git'
 
   head do
