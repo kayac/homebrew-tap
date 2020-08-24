39245d65777e69e3cfb958a9e6abc425a98c06ae
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2020-08-24 15:28:01 +0900

    bqin for linuxbrew

--- bqin.rb
+++ bqin.rb
@@ -1,8 +1,14 @@
 class Bqin < Formula
   version '0.3.1'
   homepage 'https://github.com/kayac/bqin'
-  url "https://github.com/kayac/bqin/releases/download/v0.3.1/bqin-v0.3.1-darwin-amd64"
-  sha256 '2ece07c7cec8cfb233d5958adf22a63342955d4b2749dcd745b2fbb3fce645db'
+  if OS.mac?
+    url "https://github.com/kayac/bqin/releases/download/v0.3.1/bqin-v0.3.1-darwin-amd64.zip"
+    sha256 'bbdb9c45fc8e275005214eff31f28340256d189a8bef6916e7ebfad3542371ae'
+  end
+  if OS.linux?
+    url "https://github.com/kayac/bqin/releases/download/v0.3.1/bqin-v0.3.1-linux-amd64.zip"
+    sha256 '2054684e3b8177da7f52b1ae5aadeef05a4a8fb96fd3ec49e6897a81dcda4660'
+  end
   head 'https://github.com/kayac/bqin.git'
 
   head do
@@ -13,7 +19,7 @@ class Bqin < Formula
     if build.head?
       system 'make', 'build'
     end
-    system 'mv bqin-v*-darwin-amd64 bqin'
+    system 'mv bqin-v*-*-amd64 bqin'
     bin.install 'bqin'
   end
 end
