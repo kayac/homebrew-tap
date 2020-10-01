b9e04fd30a83382cb4c54d34715795d7466cc226
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2020-10-01 12:16:30 +0900

    ecspresso v0.99.8

--- ecspresso@0.99.rb
+++ ecspresso@0.99.rb
@@ -1,14 +1,14 @@
 class EcspressoAT099 < Formula
-    version '0.99.7'
+    version '0.99.8'
     desc "This is a pre-release version of ecspresso. Fomula for a limited time"
     homepage 'https://github.com/kayac/ecspresso'
     if OS.mac?
-      url "https://github.com/kayac/ecspresso/releases/download/v0.99.7/ecspresso-v0.99.7-darwin-amd64.zip"
-      sha256 '8bc15cc9645b462a1e7345f0ed7733f88d22ee1b7046d13ae24ad888f8e1f6a7'
+      url "https://github.com/kayac/ecspresso/releases/download/v0.99.8/ecspresso-v0.99.8-darwin-amd64.zip"
+      sha256 'a53aee703fefca2f64597e350c7359e02492c2b72a0fc745409f00714357967c'
     end
     if OS.linux?
-      url "https://github.com/kayac/ecspresso/releases/download/v0.99.7/ecspresso-v0.99.7-linux-amd64.zip"
-      sha256 'bbe16ddfe47c86f727042e7d17903bd14be376a5df7ad2a8bbf68f7815388eb3'
+      url "https://github.com/kayac/ecspresso/releases/download/v0.99.8/ecspresso-v0.99.8-linux-amd64.zip"
+      sha256 '4f948dc009b316c3ce7d3055d09cd1381b5b57f1f17cef10caebfe5b39ebf054'
     end
     head 'https://github.com/kayac/ecspresso.git'
 
