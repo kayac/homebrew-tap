0f06eea9b372fb9f71ceadbadc7637aeb96f362c
ossbot <oss@noreply.kayac.com>
2020-11-05 09:33:14 +0000

    update to newer version

--- gunfish.rb
+++ gunfish.rb
@@ -1,13 +1,13 @@
 class Gunfish < Formula
-  version '0.4.1'
+  version '0.4.2'
   homepage 'https://github.com/kayac/gunfish'
   if OS.mac?
-    url "https://github.com/kayac/Gunfish/releases/download/v0.4.1/gunfish-v0.4.1-darwin-amd64.zip"
-    sha256 'cadad30566cfd4a9190411b981b483b1ab6e153fbe22a97b40674c17875ca4ec'
+    url "https://github.com/kayac/Gunfish/releases/download/v0.4.2/gunfish-v0.4.2-darwin-amd64.zip"
+    sha256 '700ae93f0e75aee7922b471278963c3685ccc00a96ce8bb8acee5a9977e58c6a'
   end
   if OS.linux?
-    url "https://github.com/kayac/Gunfish/releases/download/v0.4.1/gunfish-v0.4.1-linux-amd64.zip"
-    sha256 '23f3ca1c18664079fc4142b42112eeabef5af4ddf127f56b54dc34fbc7329b85'
+    url "https://github.com/kayac/Gunfish/releases/download/v0.4.2/gunfish-v0.4.2-linux-amd64.zip"
+    sha256 '4c5f2743d9ba751fd22f4bfb779eef52d0243923ba36072bf89528f5c717fc37'
   end
   head 'https://github.com/kayac/gunfish.git'
 
