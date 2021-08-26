d77ca68f72a0aa4f5603bd13c8a0e9af21bfbdea
ossbot <oss@noreply.kayac.com>
2021-08-26 09:32:18 +0000

    update to newer version

--- s3-object-router.rb
+++ s3-object-router.rb
@@ -1,13 +1,13 @@
 class S3ObjectRouter < Formula
-  version '0.0.6'
+  version '0.0.7'
   homepage 'https://github.com/kayac/s3-object-router'
   if OS.mac?
-    url "https://github.com/kayac/s3-object-router/releases/download/v0.0.6/s3-object-router_0.0.6_darwin_amd64.tar.gz"
-    sha256 '306dc471e0d69bb423178d300e6f42ce2bc8c3783d5fd15c0e89a1bf7207899d'
+    url "https://github.com/kayac/s3-object-router/releases/download/v0.0.7/s3-object-router_0.0.7_darwin_amd64.tar.gz"
+    sha256 '3994133ecafe8ba37fd67e4a499cd4ed255105d9e4a6fd17556c5fe5723caf24'
   end
   if OS.linux?
-    url "https://github.com/kayac/s3-object-router/releases/download/v0.0.6/s3-object-router_0.0.6_linux_amd64.tar.gz"
-    sha256 'e329cbf0c6c2fb274f63259fb69004d6377b68fc35dadef005a78ad6497acd4b'
+    url "https://github.com/kayac/s3-object-router/releases/download/v0.0.7/s3-object-router_0.0.7_linux_amd64.tar.gz"
+    sha256 '13a1f07f2247afe9cf4b852a7afee065321de4345b0b35008b8883aa6dc2ea0a'
   end
   head 'https://github.com/kayac/s3-object-router.git'
 
