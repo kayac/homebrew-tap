0b42bcdba7c920fc8856085112c49b336b3dc295
ossbot <oss@noreply.kayac.com>
2021-10-29 15:10:33 +0000

    update to newer version

--- ecspresso.rb
+++ ecspresso.rb
@@ -1,13 +1,13 @@
 class Ecspresso < Formula
-  version '1.6.2'
+  version '1.7.0'
   homepage 'https://github.com/kayac/ecspresso'
   if OS.mac?
-    url "https://github.com/kayac/ecspresso/releases/download/v1.6.2/ecspresso_1.6.2_darwin_amd64.tar.gz"
-    sha256 '887a95bd724c36837480a95982ac4293d971e848751494831b1295992a168dbb'
+    url "https://github.com/kayac/ecspresso/releases/download/v1.7.0/ecspresso_1.7.0_darwin_amd64.tar.gz"
+    sha256 '25b38893b489aed4f49e7045bfdbfb93ab39ee5dfd733d2b9e0bccfc82683fff'
   end
   if OS.linux?
-    url "https://github.com/kayac/ecspresso/releases/download/v1.6.2/ecspresso_1.6.2_linux_amd64.tar.gz"
-    sha256 'beb7bf5e3bc335344a56df37d0203de1518a3ddf7630f150e2857998b0a4454d'
+    url "https://github.com/kayac/ecspresso/releases/download/v1.7.0/ecspresso_1.7.0_linux_amd64.tar.gz"
+    sha256 'ac7f4b2f0e62cc8b83b9c4bfe3302f7797ba56dd224425970d1f7459fc4a161d'
   end
   head 'https://github.com/kayac/ecspresso.git'
 
