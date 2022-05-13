951160597930187d8e05774ae0e298ceb7821b2e
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2022-05-14 02:04:36 +0900

    ecspresso v1.7.11

--- ecspresso.rb
+++ ecspresso.rb
@@ -1,13 +1,13 @@
 class Ecspresso < Formula
-  version '1.7.10'
+  version '1.7.11'
   homepage 'https://github.com/kayac/ecspresso'
   if OS.mac?
-    url "https://github.com/kayac/ecspresso/releases/download/v1.7.10/ecspresso_1.7.10_darwin_amd64.tar.gz"
-    sha256 '18424403c960c632e113064581e48a75f8955478466f0d54ba41efdb18056fee'
+    url "https://github.com/kayac/ecspresso/releases/download/v1.7.11/ecspresso_1.7.11_darwin_amd64.tar.gz"
+    sha256 '9d0860b5fcde0fc0af28a029d4ea65f1565ea96ba21f4db02090dbc00b209e8c'
   end
   if OS.linux?
-    url "https://github.com/kayac/ecspresso/releases/download/v1.7.10/ecspresso_1.7.10_linux_amd64.tar.gz"
-    sha256 '2538b3af472690f9d75da387ba8b3fbd516a5ecfe5219d68aecbd9f56cf4f559'
+    url "https://github.com/kayac/ecspresso/releases/download/v1.7.11/ecspresso_1.7.11_linux_amd64.tar.gz"
+    sha256 '8767e551c853863caae9b21ddc80c3dc8142d280236debbcc17dba54cc4dc9a4'
   end
   head 'https://github.com/kayac/ecspresso.git'
 
