a473279a6239bd20e16807373ab4b6d262808172
ossbot <oss@noreply.kayac.com>
2022-11-14 06:15:57 +0000

    update to newer version

--- ecspresso.rb
+++ ecspresso.rb
@@ -1,27 +1,27 @@
 class Ecspresso < Formula
   desc 'ecspresso is a deployment tool for Amazon ECS'
-  version '1.7.13'
+  version '1.7.14'
   homepage 'https://github.com/kayac/ecspresso'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/kayac/ecspresso/releases/download/v1.7.13/ecspresso_1.7.13_darwin_arm64.tar.gz'
-      sha256 'ef2cb00246a3da2f2ed9897cf19d4c813b7c9947dcc526daea1ea9323edc7ea2'
+      url 'https://github.com/kayac/ecspresso/releases/download/v1.7.14/ecspresso_1.7.14_darwin_arm64.tar.gz'
+      sha256 '60de61f14111acbae24e0d3c32847935fc704f231dc9cd983435349527d84f78'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/kayac/ecspresso/releases/download/v1.7.13/ecspresso_1.7.13_darwin_amd64.tar.gz'
-      sha256 '5aa341affd2e821d024829303de0753af8231b741709e0ca0c159b401d0ad9c6'
+      url 'https://github.com/kayac/ecspresso/releases/download/v1.7.14/ecspresso_1.7.14_darwin_amd64.tar.gz'
+      sha256 'bba9f47e76c9c9b0d6ceab735b76eea1f5eb7a550c8a4947f4999bca0eec9e13'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/kayac/ecspresso/releases/download/v1.7.13/ecspresso_1.7.13_linux_arm64.tar.gz'
-      sha256 '5a165e76e03d0bb391161fe71af0b610cd1e82ac024f3fbbfa39be361dafd4d6'
+      url 'https://github.com/kayac/ecspresso/releases/download/v1.7.14/ecspresso_1.7.14_linux_arm64.tar.gz'
+      sha256 '51c24a8f224d0a4e0ac08e78d6db2307212ee46d561473d3b89aec49833724a4'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/kayac/ecspresso/releases/download/v1.7.13/ecspresso_1.7.13_linux_amd64.tar.gz'
-      sha256 'd30b91019263b08efed062f0cfd2e7af40afc055b231ab57f20749b00ede705f'
+      url 'https://github.com/kayac/ecspresso/releases/download/v1.7.14/ecspresso_1.7.14_linux_amd64.tar.gz'
+      sha256 '4562579c5bba7acd46c294eaac8a57be596afe80c79c9c3c1649a9e02958223b'
     end
   end
 
