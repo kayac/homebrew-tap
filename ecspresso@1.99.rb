b896f65bec66e18775c3d318468b592afc502402
fujiwara <fujiwara.shunichiro@gmail.com>
2022-11-25 17:00:00 +0000

    ecspresso v1.99.5

--- ecspresso@1.99.rb
+++ ecspresso@1.99.rb
@@ -1,27 +1,27 @@
 class EcspressoAT199 < Formula
   desc 'ecspresso is a deployment tool for Amazon ECS'
-  version '1.99.4'
+  version '1.99.5'
   homepage 'https://github.com/kayac/ecspresso'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/kayac/ecspresso/releases/download/v1.99.4/ecspresso_1.99.4_darwin_arm64.tar.gz'
-      sha256 '1eb40cb52a5cff6458373b19f1e82acfd010d9143b6f065ce89946adcf59da75'
+      url 'https://github.com/kayac/ecspresso/releases/download/v1.99.5/ecspresso_1.99.5_darwin_arm64.tar.gz'
+      sha256 '9b2d4717c7d857058163f5365dafa86653894a00102fd0c6934145b3f78b10f5'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/kayac/ecspresso/releases/download/v1.99.4/ecspresso_1.99.4_darwin_amd64.tar.gz'
-      sha256 '096e9cb1f007faab24fc342f9b647248eeda54784c739cefde32d20faf5cdad2'
+      url 'https://github.com/kayac/ecspresso/releases/download/v1.99.5/ecspresso_1.99.5_darwin_amd64.tar.gz'
+      sha256 '819e30b56a1e6b3dd67fd450ebde90233a17e3d54497c03dd770b4736aa90a54'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/kayac/ecspresso/releases/download/v1.99.4/ecspresso_1.99.4_linux_arm64.tar.gz'
-      sha256 'c923d69aa74c7dcbe2a18a56c3efd7d931efcad35db00f68b2daf9f4db7701ed'
+      url 'https://github.com/kayac/ecspresso/releases/download/v1.99.5/ecspresso_1.99.5_linux_arm64.tar.gz'
+      sha256 'd873357286f73ec4bbd59ded6353587bcddcacd31e0f71d7fe46d3b75b606579'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/kayac/ecspresso/releases/download/v1.99.4/ecspresso_1.99.4_linux_amd64.tar.gz'
-      sha256 'ed4f6fce6518251c2b35771c2c3f5b77e44ac06967c058c055fe8b61dcd8de36'
+      url 'https://github.com/kayac/ecspresso/releases/download/v1.99.5/ecspresso_1.99.5_linux_amd64.tar.gz'
+      sha256 '3482c905a043d3440b4a54e2f84e9f0256a7c36eeb2c510836ba72778f7125a6'
     end
   end
 
