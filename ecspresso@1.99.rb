147ca4dcd5b7bcc526b2cfcaa7191d6ae7c1191c
fujiwara <fujiwara.shunichiro@gmail.com>
2022-11-25 06:57:35 +0000

    ecspresso v1.99.4

--- ecspresso@1.99.rb
+++ ecspresso@1.99.rb
@@ -1,27 +1,27 @@
 class EcspressoAT199 < Formula
   desc 'ecspresso is a deployment tool for Amazon ECS'
-  version '1.99.3'
+  version '1.99.4'
   homepage 'https://github.com/kayac/ecspresso'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/kayac/ecspresso/releases/download/v1.99.3/ecspresso_1.99.3_darwin_arm64.tar.gz'
-      sha256 '09b16d03c5c9307e2b9b00522578af946a2c3ca2e38089dfed320379fdcd2bc4'
+      url 'https://github.com/kayac/ecspresso/releases/download/v1.99.4/ecspresso_1.99.4_darwin_arm64.tar.gz'
+      sha256 '1eb40cb52a5cff6458373b19f1e82acfd010d9143b6f065ce89946adcf59da75'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/kayac/ecspresso/releases/download/v1.99.3/ecspresso_1.99.3_darwin_amd64.tar.gz'
-      sha256 '48c2caaa129a7f0bafd4c63978661c7b2af95a34cd972d359cde36f505b04983'
+      url 'https://github.com/kayac/ecspresso/releases/download/v1.99.4/ecspresso_1.99.4_darwin_amd64.tar.gz'
+      sha256 '096e9cb1f007faab24fc342f9b647248eeda54784c739cefde32d20faf5cdad2'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/kayac/ecspresso/releases/download/v1.99.3/ecspresso_1.99.3_linux_arm64.tar.gz'
-      sha256 '8a515a2345a7f2facb1ca42543569fd5e7f44fc705867398b1cff0daaf0fc0e7'
+      url 'https://github.com/kayac/ecspresso/releases/download/v1.99.4/ecspresso_1.99.4_linux_arm64.tar.gz'
+      sha256 'c923d69aa74c7dcbe2a18a56c3efd7d931efcad35db00f68b2daf9f4db7701ed'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/kayac/ecspresso/releases/download/v1.99.3/ecspresso_1.99.3_linux_amd64.tar.gz'
-      sha256 '5955438b05395b4f28effa2d962f28ab8a17a7413e279edf0f3de06a39171dd2'
+      url 'https://github.com/kayac/ecspresso/releases/download/v1.99.4/ecspresso_1.99.4_linux_amd64.tar.gz'
+      sha256 'ed4f6fce6518251c2b35771c2c3f5b77e44ac06967c058c055fe8b61dcd8de36'
     end
   end
 
