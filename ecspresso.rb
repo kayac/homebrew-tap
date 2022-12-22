13b6211cd1b0cf32ff25aa82cf4c46f14c47d6a8
fujiwara <fujiwara.shunichiro@gmail.com>
2022-12-22 08:40:49 +0000

    ecspresso v2.0.3

--- ecspresso.rb
+++ ecspresso.rb
@@ -1,27 +1,27 @@
 class Ecspresso < Formula
   desc 'ecspresso is a deployment tool for Amazon ECS'
-  version '2.0.1'
+  version '2.0.3'
   homepage 'https://github.com/kayac/ecspresso'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/kayac/ecspresso/releases/download/v2.0.1/ecspresso_2.0.1_darwin_arm64.tar.gz'
-      sha256 '5f74f944541a4113baba5929266fd8bfd1f78c4e6d5da594d74b1c7695b67c40'
+      url 'https://github.com/kayac/ecspresso/releases/download/v2.0.3/ecspresso_2.0.3_darwin_arm64.tar.gz'
+      sha256 'f28e055a142d7deeecf255fe60753414c5e1c278a5acfc66bcb5bb8e410a1d9d'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/kayac/ecspresso/releases/download/v2.0.1/ecspresso_2.0.1_darwin_amd64.tar.gz'
-      sha256 'd7f7f97018fcea91b8ce70414505dbe573bf4a9f74b6a121ab2d69520d5a81ee'
+      url 'https://github.com/kayac/ecspresso/releases/download/v2.0.3/ecspresso_2.0.3_darwin_amd64.tar.gz'
+      sha256 '0ad1c5b23b3d0256df6335da3b3dc21805d5080d26fc180b5f38b7e4a4d944bd'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/kayac/ecspresso/releases/download/v2.0.1/ecspresso_2.0.1_linux_arm64.tar.gz'
-      sha256 'ecba96e0331810d46bd77d55b7455bd818a9fde045ae1ab1faa1dcd6dcc28697'
+      url 'https://github.com/kayac/ecspresso/releases/download/v2.0.3/ecspresso_2.0.3_linux_arm64.tar.gz'
+      sha256 '6396a4f0ddcc03adf986e920d86477583b820c51c27bacc5f46ca9dd352b710d'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/kayac/ecspresso/releases/download/v2.0.1/ecspresso_2.0.1_linux_amd64.tar.gz'
-      sha256 'd69a974c094da09f04540e3641e84a3817b9dba7d05d2cae4b9c5d14f16b7062'
+      url 'https://github.com/kayac/ecspresso/releases/download/v2.0.3/ecspresso_2.0.3_linux_amd64.tar.gz'
+      sha256 '15bc0ac01976679e34fca851d83d5645d78142b34c8091b1649d27d0cd0bce21'
     end
   end
 
