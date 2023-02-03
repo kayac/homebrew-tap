b5d0bdb19089b0f461b51270de8c292a2378fa14
fujiwara <fujiwara.shunichiro@gmail.com>
2023-02-04 01:36:47 +0900

    ecspresso v2.0.4

--- ecspresso.rb
+++ ecspresso.rb
@@ -1,27 +1,27 @@
 class Ecspresso < Formula
   desc 'ecspresso is a deployment tool for Amazon ECS'
-  version '2.0.3'
+  version '2.0.4'
   homepage 'https://github.com/kayac/ecspresso'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/kayac/ecspresso/releases/download/v2.0.3/ecspresso_2.0.3_darwin_arm64.tar.gz'
-      sha256 'f28e055a142d7deeecf255fe60753414c5e1c278a5acfc66bcb5bb8e410a1d9d'
+      url 'https://github.com/kayac/ecspresso/releases/download/v2.0.4/ecspresso_2.0.4_darwin_arm64.tar.gz'
+      sha256 '74fe8cd3664e4f6e8c648fe96f56e09fcc11aededea9c018bd8940822c4a9404'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/kayac/ecspresso/releases/download/v2.0.3/ecspresso_2.0.3_darwin_amd64.tar.gz'
-      sha256 '0ad1c5b23b3d0256df6335da3b3dc21805d5080d26fc180b5f38b7e4a4d944bd'
+      url 'https://github.com/kayac/ecspresso/releases/download/v2.0.4/ecspresso_2.0.4_darwin_amd64.tar.gz'
+      sha256 '0ae5c170136031d6030da18b17ec09c166fe80f78e6b82e44d26d2d9fecb1f7c'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/kayac/ecspresso/releases/download/v2.0.3/ecspresso_2.0.3_linux_arm64.tar.gz'
-      sha256 '6396a4f0ddcc03adf986e920d86477583b820c51c27bacc5f46ca9dd352b710d'
+      url 'https://github.com/kayac/ecspresso/releases/download/v2.0.4/ecspresso_2.0.4_linux_arm64.tar.gz'
+      sha256 '9c1ee82eba1d130bdc58f6f0de43bf47034a7cb347b30ead44a859d74c97cdcf'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/kayac/ecspresso/releases/download/v2.0.3/ecspresso_2.0.3_linux_amd64.tar.gz'
-      sha256 '15bc0ac01976679e34fca851d83d5645d78142b34c8091b1649d27d0cd0bce21'
+      url 'https://github.com/kayac/ecspresso/releases/download/v2.0.4/ecspresso_2.0.4_linux_amd64.tar.gz'
+      sha256 'fe0fe1336bd56d0a2568462f0250effaa614322e39818384c541d88edc412b4c'
     end
   end
 
