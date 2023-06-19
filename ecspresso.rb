242a81e34631eee65840ac9923962b93e76e30dd
ossbot <oss@noreply.kayac.com>
2023-06-19 08:53:25 +0000

    update to newer version

--- ecspresso.rb
+++ ecspresso.rb
@@ -1,27 +1,27 @@
 class Ecspresso < Formula
   desc 'ecspresso is a deployment tool for Amazon ECS'
-  version '2.2.0'
+  version '2.2.1'
   homepage 'https://github.com/kayac/ecspresso'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/kayac/ecspresso/releases/download/v2.2.0/ecspresso_2.2.0_darwin_arm64.tar.gz'
-      sha256 '227852315550d3ca131e3fc69d568509f4e62a6288de57a136ec4382f8bbea7b'
+      url 'https://github.com/kayac/ecspresso/releases/download/v2.2.1/ecspresso_2.2.1_darwin_arm64.tar.gz'
+      sha256 '4ec7688bfc2cbde5b2c9dccb534712d084ed853469372e94f193a495bf804cbb'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/kayac/ecspresso/releases/download/v2.2.0/ecspresso_2.2.0_darwin_amd64.tar.gz'
-      sha256 '5125f877fbd9eea7ef245c6312ae603d262c12df75eae5201e1925b91eb10a18'
+      url 'https://github.com/kayac/ecspresso/releases/download/v2.2.1/ecspresso_2.2.1_darwin_amd64.tar.gz'
+      sha256 '3fb515df4beefb4d78f5eb1ac6e7693fce34e7c1b9e57d9b386acc2456456b5c'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/kayac/ecspresso/releases/download/v2.2.0/ecspresso_2.2.0_linux_arm64.tar.gz'
-      sha256 '2b53a9a46625e34b24301c0fcb823d481f971ea8121c0718bb721e287765bc7a'
+      url 'https://github.com/kayac/ecspresso/releases/download/v2.2.1/ecspresso_2.2.1_linux_arm64.tar.gz'
+      sha256 '59fae863d006744acbc8f92a9c3b0d7195c6d922c2456242094ee7d2219cfe59'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/kayac/ecspresso/releases/download/v2.2.0/ecspresso_2.2.0_linux_amd64.tar.gz'
-      sha256 '73f2054af3dd2d966cce24c13ad05da31b82123551235d8f6a2ea67d92f6e147'
+      url 'https://github.com/kayac/ecspresso/releases/download/v2.2.1/ecspresso_2.2.1_linux_amd64.tar.gz'
+      sha256 '6c2ccc53c9ab841f8384bbd44c7dace3f54f489d52d6542a6fabdf4f5dd4af81'
     end
   end
 
