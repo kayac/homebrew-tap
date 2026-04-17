76c067441a9d4850f5285c2af796392916add5b6
ossbot <oss@noreply.kayac.com>
2026-04-17 14:40:53 +0000

    update to newer version

--- ecspresso.rb
+++ ecspresso.rb
@@ -1,27 +1,27 @@
 class Ecspresso < Formula
   desc 'ecspresso is a deployment tool for Amazon ECS'
-  version '2.8.0'
+  version '2.8.1'
   homepage 'https://github.com/kayac/ecspresso'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/kayac/ecspresso/releases/download/v2.8.0/ecspresso_2.8.0_darwin_arm64.tar.gz'
-      sha256 '2118d4e20cf7266d5fbdb48bb4a9087cd9257214e8fda3c47ee1f879eb717926'
+      url 'https://github.com/kayac/ecspresso/releases/download/v2.8.1/ecspresso_2.8.1_darwin_arm64.tar.gz'
+      sha256 'd909d7cd84acb9a4996808c118cdb8df06ac4aa1b63e939c9aaf7248c39a3a76'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/kayac/ecspresso/releases/download/v2.8.0/ecspresso_2.8.0_darwin_amd64.tar.gz'
-      sha256 '04d9840f2732a33d47adb014217cc943459caa249dee8da5e7e9e089ecd4c84f'
+      url 'https://github.com/kayac/ecspresso/releases/download/v2.8.1/ecspresso_2.8.1_darwin_amd64.tar.gz'
+      sha256 'c4624eb7cbed75aaf15d9a573efc6fc6370d3b2342daf49cd0fe026c91a82a34'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/kayac/ecspresso/releases/download/v2.8.0/ecspresso_2.8.0_linux_arm64.tar.gz'
-      sha256 '3373ac1507fa09800a951ebb565dd1ded3123394014c75f9412131c0ca63426e'
+      url 'https://github.com/kayac/ecspresso/releases/download/v2.8.1/ecspresso_2.8.1_linux_arm64.tar.gz'
+      sha256 'bb178370081fe52dbfc2eaad938b147246874a782e6f17a5dfbdeaad84852392'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/kayac/ecspresso/releases/download/v2.8.0/ecspresso_2.8.0_linux_amd64.tar.gz'
-      sha256 '7f9a7a0f52dff219d89a61bd1c545124fe1c3d259f677aee0d9eb0393aabc4a7'
+      url 'https://github.com/kayac/ecspresso/releases/download/v2.8.1/ecspresso_2.8.1_linux_amd64.tar.gz'
+      sha256 '521dd3689c9e4482d6073c940b6b387ef9561144f750fdd92e33d5b7f9a03427'
     end
   end
 
