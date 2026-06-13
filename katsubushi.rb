e0e50bf2829a32f9ef885f592db3b5ab9dfd1c33
ossbot <oss@noreply.kayac.com>
2026-06-13 01:29:30 +0000

    update to newer version

--- katsubushi.rb
+++ katsubushi.rb
@@ -1,27 +1,27 @@
 class Katsubushi < Formula
   desc 'ID generator server'
-  version '2.2.1'
+  version '2.3.0'
   homepage 'https://github.com/kayac/go-katsubushi'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/kayac/go-katsubushi/releases/download/v2.2.1/go-katsubushi_v2.2.1_darwin_arm64.tar.gz'
-      sha256 '5f126852ff6f4b3613375c86ddcd51f59fe918e623b14452dddb36110e3c13cb'
+      url 'https://github.com/kayac/go-katsubushi/releases/download/v2.3.0/go-katsubushi_v2.3.0_darwin_arm64.tar.gz'
+      sha256 '696fc63918c268f475181f1fddd7891e2fd9f5998ad47ceb8eca740c59733dec'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/kayac/go-katsubushi/releases/download/v2.2.1/go-katsubushi_v2.2.1_darwin_amd64.tar.gz'
-      sha256 'aa1aa6e23bcf5407bce782f34f957c2d42a7ed6690bb9f25188b076f43533519'
+      url 'https://github.com/kayac/go-katsubushi/releases/download/v2.3.0/go-katsubushi_v2.3.0_darwin_amd64.tar.gz'
+      sha256 '8e71fcffe6fca3b3f8ab7d00381d7dda3d40f445fdcdd69dc5d919ad78d65b04'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/kayac/go-katsubushi/releases/download/v2.2.1/go-katsubushi_v2.2.1_linux_arm64.tar.gz'
-      sha256 'ec23a05acb2b268dfa34a9989d74cfbcbb4b1f631ac082aa5ad53f3af5f12134'
+      url 'https://github.com/kayac/go-katsubushi/releases/download/v2.3.0/go-katsubushi_v2.3.0_linux_arm64.tar.gz'
+      sha256 '40aa4d7967159856c7681bde7b43e12e4a247084ea0740d2b1e8554ff83bf40f'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/kayac/go-katsubushi/releases/download/v2.2.1/go-katsubushi_v2.2.1_linux_amd64.tar.gz'
-      sha256 '4b34c6c8a2d0a14678864876f75e85c90402e9b9e5cdd7d0150a8537403ea5e2'
+      url 'https://github.com/kayac/go-katsubushi/releases/download/v2.3.0/go-katsubushi_v2.3.0_linux_amd64.tar.gz'
+      sha256 '09607230fe3d0ed31c5c45c0831eb8870c9f7412a2b47936e544cecb5750bd71'
     end
   end
 
